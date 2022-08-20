c6283ddf4fdb5635c4f5b739ae6f12eb4ce5bc36
davidkotschessa <david.kotschessa@crayon.co>
2022-08-20 19:24:29 -0400

    doing some leftover exercises

---
+++ misc_exercises.js
@@ -0,0 +1,53 @@
+console.log('yup it\'s working')
+
+class Group {
+    constructor() {
+        this.values = Array()
+    }
+    add(value) {
+        if (!this.values.includes(value)) {
+            return this.values.push(value)
+        }
+
+    }
+    delete(value) {
+    if (this.values.includes(value)) {
+        index = this.values.indexOf(value)
+        return this.values.splice(value, 1)
+    }
+    }
+    has(value) {
+        return (this.values.includes(value))
+    };
+    from(iterable) {
+        for (i of iterable) {
+            this.add(i)
+        }
+    }
+
+    }
+
+
+
+
+
+class MatrixIterator {
+  constructor(matrix) {
+    this.x = 0
+    this.y = 0
+    this.matrix = matrix
+  }
+
+  next() {
+    if (this.y == this.matrix.height) return { done: true }
+
+    let value = { x: this.x, y: this.y, value: this.matrix.get(this.x, this.y) }
+    this.x++
+
+    if (this.x == this.matrix.width) {
+      this.x = 0
+      this.y++
+    }
+    return { value, done: false }
+  }
+}
